ff57e49a41f79557686910468426b03e414e9070
Taras <tonyjoss1990@gmail.com>
2023-12-05 23:16:01 -0500

    remove unnecessary code

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
 const express = require('express');
-// const fs = require('fs');
-// const { logUsers } = require('./controllers/readFiles.js');
-// const userApiPath = './api/users.json';
 const { errorHandlers } = require('./middleware');
 const { taskController, readUserController } = require('./controllers');
-//const { readUserController } = require('./controllers');
 
 
-// const {
-//   createTask,
-//   getTasks,
-//   getTaskById,
-//   updateTaskById,
-//   removeTaskById,
-// } = require('./controllers/taskControllers');
+
+
 
 const app = express();
 
 app.use(express.json());
-
-// app.get('/', (req, res) => {
-//   try {
-//       const users = fs.readFileSync(userApiPath, { encoding: 'utf-8' });
-//       const usersObj = JSON.parse(users);
-//       res.status(200).send(usersObj);
-//   } catch (error) {
-//       res.status(500).json({status: error, msg: 'Internal Server Error'})
-//   }
-//   // async method
-//   logUsers(userApiPath)
-// });
-
 
 
 
